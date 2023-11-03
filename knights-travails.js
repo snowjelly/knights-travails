765507eb60e933e6aaa450c7f7d280630736e4cb
snowjelly <broadwellconnor@gmail.com>
2023-11-03 13:29:07 -0500

    choosing largest move to make

--- knights-travails.js
+++ knights-travails.js
@@ -11,7 +11,9 @@ const node = (x, y) => {
     arr.push([x - 1, y + 2]);
     arr.push([x - 1, y - 2]);
     arr.push([x + 1, y - 2]);
-    const legalMoves = arr.filter((move) => move[0] > 0 && move[1] > 0);
+    const legalMoves = arr.filter(
+      (move) => move[0] > 0 && move[0] <= 7 && move[1] > 0 && move[1] <= 7
+    );
     legalMoves.sort((a, b) => {
       if (a[0] - b[0] === 0) {
         return a[1] - b[1];
@@ -29,15 +31,15 @@ const node = (x, y) => {
   };
 };
 
-function buildTree(l, h, arr, root) {
+function buildTree(l, h, arr) {
   if (l > h) return null;
   let mid = Math.floor((l + h) / 2);
   const x = arr[mid][0];
   const y = arr[mid][1];
 
   const nodeOb = node(x, y);
-  nodeOb.left = buildTree(l, mid - 1, arr, root);
-  nodeOb.right = buildTree(mid + 1, h, arr, root);
+  nodeOb.left = buildTree(l, mid - 1, arr);
+  nodeOb.right = buildTree(mid + 1, h, arr);
   return nodeOb;
 }
 
@@ -66,6 +68,34 @@ function inorder(cb = null, queue = [root], orderedArray = []) {
     return orderedArray;
   }
 }
+function find(value, rootNode) {
+  const x = value[0];
+  const y = value[1];
+  while (x !== rootNode.data[0]) {
+    if (x < rootNode.data[0] && rootNode.left !== null) {
+      rootNode = rootNode.left;
+    } else if (x > rootNode.data[0] && rootNode.right !== null) {
+      rootNode = rootNode.right;
+    } else {
+      break;
+    }
+  }
+  while (y !== rootNode.data[1]) {
+    if (y < rootNode.data[1] && rootNode.left !== null) {
+      rootNode = rootNode.left;
+    } else if (y > rootNode.data[1] && rootNode.right !== null) {
+      rootNode = rootNode.right;
+    } else {
+      break;
+    }
+  }
+
+  if (x === rootNode.data[0] && y === rootNode.data[1]) {
+    return { rootNode, found: true };
+  }
+
+  return rootNode;
+}
 
 function knightMoves(start, destination) {
   // knight can move 2 steps forward and one to the side
@@ -85,27 +115,45 @@ function knightMoves(start, destination) {
   const startNode = node(startX, startY);
   const destinationNode = node(goalX, goalY);
 
-  const rootNode = buildTree(
+  let rootNode = buildTree(
     0,
     startNode.potentialMoves.length - 1,
-    startNode.potentialMoves,
-    destinationNode.data
+    startNode.potentialMoves
   );
 
   prettyPrint(rootNode);
-  inorder(
-    (value) => {
-      if (
-        destinationNode.data[0] === value.data[0] &&
-        destinationNode.data[1] === value.data[1]
-      ) {
-        const counter = 1;
-        console.log(`You made it in ${counter} move!  Here's your path:`);
-        console.log(value.data);
-      }
-    },
-    [rootNode]
-  );
+
+  function move(rootNode) {
+    let n = find(destinationNode.data, rootNode);
+    if (n.found) {
+      console.log("found");
+      console.log(n.rootNode);
+      return;
+    }
+
+    // console.log({ n: n.potentialMoves });
+
+    rootNode = buildTree(0, n.potentialMoves.length - 1, n.potentialMoves);
+    console.log(n);
+
+    move(rootNode);
+  }
+
+  console.log(move(rootNode));
+
+  // console.log(startNode.potentialMoves);
+  // const testX = startNode.potentialMoves[1][0];
+  // const testY = startNode.potentialMoves[1][1];
+  // const testNode = node(testX, testY);
+  // console.log(testNode.potentialMoves);
+  // console.log(destinationNode.potentialMoves);
+
+  // once u get to the correct x value. u can subtract 2 and then add back two
+  //
+
+  // check both possible move lists of both destination and start.
+  // compare those. you search those. and if there isnt anything close then idk.
+
   // console.log({ startNode: startNode.potentialMoves, destinationNode });
 }
 
@@ -122,4 +170,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-console.log(knightMoves([3, 3], [4, 5]));
+console.log(knightMoves([0, 0], [7, 7]));
