b38d3726e123b4a6fc8744c08c257af1970bd84d
snowjelly <broadwellconnor@gmail.com>
2023-11-03 10:55:22 -0500

    testing traversal

--- knights-travails.js
+++ knights-travails.js
@@ -12,6 +12,12 @@ const node = (x, y) => {
     arr.push([x - 1, y - 2]);
     arr.push([x + 1, y - 2]);
     const legalMoves = arr.filter((move) => move[0] > 0 && move[1] > 0);
+    legalMoves.sort((a, b) => {
+      if (a[0] - b[0] === 0) {
+        return a[1] - b[1];
+      }
+      return a[0] - b[0];
+    });
 
     return legalMoves;
   }
@@ -23,12 +29,42 @@ const node = (x, y) => {
   };
 };
 
-function buildTree(arr, root) {
-  let l = 0;
-  let h = arr.length - 1;
+function buildTree(l, h, arr, root) {
+  if (l > h) return null;
   let mid = Math.floor((l + h) / 2);
+  const x = arr[mid][0];
+  const y = arr[mid][1];
 
-  console.log(arr[mid], root);
+  const nodeOb = node(x, y);
+  nodeOb.left = buildTree(l, mid - 1, arr, root);
+  nodeOb.right = buildTree(mid + 1, h, arr, root);
+  return nodeOb;
+}
+
+function inorder(cb = null, queue = [root], orderedArray = []) {
+  if (queue.length === 0) {
+    return;
+  }
+
+  const deQueue = queue.shift();
+
+  if (deQueue.left !== null) {
+    queue = [...queue, deQueue.left];
+    inorder(cb, queue, orderedArray);
+  }
+  if (cb !== null) {
+    cb(deQueue);
+  }
+  orderedArray.push(deQueue);
+
+  if (deQueue.right !== null) {
+    queue = [...queue, deQueue.right];
+    inorder(cb, queue, orderedArray);
+  }
+
+  if (queue.length === 0 && cb === null) {
+    return orderedArray;
+  }
 }
 
 function knightMoves(start, destination) {
@@ -49,7 +85,41 @@ function knightMoves(start, destination) {
   const startNode = node(startX, startY);
   const destinationNode = node(goalX, goalY);
 
-  console.log(buildTree(startNode.potentialMoves, destinationNode.data));
+  const rootNode = buildTree(
+    0,
+    startNode.potentialMoves.length - 1,
+    startNode.potentialMoves,
+    destinationNode.data
+  );
+
+  prettyPrint(rootNode);
+  inorder(
+    (value) => {
+      if (
+        destinationNode.data[0] === value.data[0] &&
+        destinationNode.data[1] === value.data[1]
+      ) {
+        const counter = 1;
+        console.log(`You made it in ${counter} move!  Here's your path:`);
+        console.log(value.data);
+      }
+    },
+    [rootNode]
+  );
+  // console.log({ startNode: startNode.potentialMoves, destinationNode });
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
 
 console.log(knightMoves([3, 3], [4, 5]));
