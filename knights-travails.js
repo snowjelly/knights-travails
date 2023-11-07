c7d761d419f63283288e5795a38e0c1cf191c636
snowjelly <broadwellconnor@gmail.com>
2023-11-07 12:07:15 -0600

    cooking

--- knights-travails.js
+++ knights-travails.js
@@ -70,6 +70,60 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
+const prettyPrintAlt = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrintAlt(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(
+    `${prefix}${isLeft ? "└── " : "┌── "}space: [${node.space}] [${
+      node.data[0]
+    }]`
+  );
+  if (node.left !== null) {
+    prettyPrintAlt(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+function levelOrderRecursive(cb = null, queue = [root], orderedArray = []) {
+  if (queue.length === 0 && cb === null) {
+    return orderedArray;
+  }
+  if (queue.length === 0) {
+    return;
+  }
+
+  const deQueue = queue.shift();
+  if (cb !== null) {
+    cb(deQueue);
+  }
+
+  if (deQueue.left !== null) {
+    queue = [...queue, deQueue.left];
+  }
+  if (deQueue.right !== null) {
+    queue = [...queue, deQueue.right];
+  }
+
+  if (cb !== null) {
+    levelOrderRecursive(cb, queue);
+  } else {
+    orderedArray = levelOrderRecursive(cb, queue, [...orderedArray, deQueue]);
+    return orderedArray;
+  }
+}
+
+function knightMoves(startArr, endArr = []) {
+  const startX = startArr[0];
+  const startY = startArr[1];
+  const endX = endArr[0];
+  const endY = endArr[1];
+
+  return [startX, startY];
+}
+
 const board = gameBoard();
 const boardsPotentialMoves = [];
 
@@ -83,6 +137,48 @@ const tree = buildTree(
   boardsPotentialMoves
 );
 
-prettyPrint(tree);
+const treeList = [];
+
+levelOrderRecursive(
+  (result) => {
+    const resultArr = result.data[0].potentialMoves;
+    const newTree = buildTree(0, resultArr.length - 1, resultArr);
+    newTree.space = result.data[0].data;
+    treeList.push(newTree);
+  },
+  [tree]
+);
+
+const dataArr = [];
+
+const knightLocation = knightMoves([3, 3]);
+
+function buildDataArr(arr, dataArr) {
+  dataArr.push(arr);
+}
+
+function findKnightInTree(cb = null, knightLoc = knightLocation) {
+  treeList.forEach((tre) => {
+    if (tre.space[0] === knightLoc[0] && tre.space[1] === knightLoc[1]) {
+      prettyPrintAlt(tre);
+      return cb(tre);
+    }
+    //console.log(tre);
+    //buildDataArr(tre.data[0], dataArr);
+  });
+}
+
+const treeStart = findKnightInTree((tre) => {
+  levelOrderRecursive(
+    (result) => {
+      result.potentialMoves = node(
+        result.data[0][0],
+        result.data[0][1]
+      ).potentialMoves;
+      console.log(result);
+    },
+    [tre]
+  );
+});
 
 // console.log(tree.data[0].potentialMoves);
