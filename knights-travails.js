d4eb12ee9876cfb75b054a558e1cbd4882f92f98
snowjelly <broadwellconnor@gmail.com>
2023-11-07 10:26:45 -0600

    gameboard

--- knights-travails.js
+++ knights-travails.js
@@ -1,5 +1,5 @@
 const node = (x, y) => {
-  function potentialMoves(x, y) {
+  function getPotentialMoves(x, y) {
     const arr = [];
 
     arr.push([x + 2, y + 1]);
@@ -20,23 +20,69 @@ const node = (x, y) => {
       }
       return a[0] - b[0];
     });
-
-    function buildTree(
-      l = 0,
-      h = sortedArray.length - 1,
-      sortedArr = sortedArray
-    ) {
-      if (l > h) return null;
-
-      const mid = Math.floor((l + h) / 2);
-      const nodeOb = node(sortedArr[mid]);
-
-      nodeOb.left = buildTree(l, mid - 1, sortedArr);
-      nodeOb.right = buildTree(mid + 1, h, sortedArr);
-      return nodeOb;
-    }
-
     return legalMoves;
   }
-  return { data: [x, y], potentialMoves };
+
+  return {
+    data: [x, y],
+    potentialMoves: getPotentialMoves(x, y),
+  };
 };
+
+function gameBoard() {
+  function create(n = 7) {
+    const arr = [];
+    for (let i = 0; i <= n; i++) {
+      for (let z = 0; z <= n; z++) {
+        arr.push([i, z]);
+      }
+    }
+    return arr;
+  }
+  return create();
+}
+
+function buildTree(l, h, sortedArr) {
+  if (l > h) return null;
+
+  const mid = Math.floor((l + h) / 2);
+  const nodeOb = node(sortedArr[mid]);
+
+  nodeOb.left = buildTree(l, mid - 1, sortedArr);
+  nodeOb.right = buildTree(mid + 1, h, sortedArr);
+  return nodeOb;
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(
+    `${prefix}${isLeft ? "└── " : "┌── "}node: [${
+      node.data[0].data
+    }] pMoves: [${node.data[0].potentialMoves}]`
+  );
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const board = gameBoard();
+const boardsPotentialMoves = [];
+
+board.forEach((space) => {
+  boardsPotentialMoves.push(node(space[0], space[1]));
+});
+
+const tree = buildTree(
+  0,
+  boardsPotentialMoves.length - 1,
+  boardsPotentialMoves
+);
+
+prettyPrint(tree);
+
+// console.log(tree.data[0].potentialMoves);
