8c3cb7f252c68b78c33202e6895a944f690f706c
snowjelly <broadwellconnor@gmail.com>
2024-01-22 14:21:54 -0600

    travel helper

--- knights-travails.js
+++ knights-travails.js
@@ -3,7 +3,7 @@ const ChessSpace = (x, y) => {
   const yPos = y;
   let prev = null;
   let distance = 0;
-  const name = `${xPos}${yPos}`;
+  const name = [xPos, yPos];
 
   const moveOffsets = [
     [1, 2],
@@ -35,35 +35,67 @@ const ChessSpace = (x, y) => {
   return { xPos, yPos, prev, knightMoves, name, knightMoveList, distance };
 };
 
-const starting = ChessSpace(3, 3);
-
-const queue = [starting];
-const storage = new Map();
-let distCounter = 0;
-queue[0].distance = 0;
-
-while (queue.length !== 0) {
-  let shifted = queue.shift();
-  if (storage.has(shifted.name) === true) {
-    shifted = queue.shift();
+function travelHelper(dest) {
+  let counter = 0;
+  const moveOrderInReverse = [];
+  while (dest.prev !== null) {
+    moveOrderInReverse.push(dest.prev.name);
+    counter += 1;
+    dest.prev = dest.prev.prev;
   }
-
-  const moveList = shifted.knightMoveList();
-  distCounter++;
-
-  if (queue.length === 1) {
-    distCounter = 0;
+  console.log(`You made it in ${counter} moves! Here's your path:`);
+  for (let i = moveOrderInReverse.length - 1; i >= 0; i--) {
+    console.log(moveOrderInReverse[i]);
   }
-
-  for (let i = 0; i < moveList.length; i++) {
-    if (moveList[i] === undefined) continue;
-    moveList[i].distance = distCounter;
-    moveList[i].prev = shifted;
-    moveList[i].traveled = true;
-    storage.set(moveList[i].name, [moveList[i].xPos, moveList[i].yPos]);
-    queue.push(moveList[i]);
-  }
-  if (storage.size === 64) break;
+  console.log(dest.name);
 }
 
-console.log(storage.values());
+function knightsTravails([startX, startY], [endX, endY]) {
+  if (
+    startX > 7 ||
+    startY > 7 ||
+    endX > 7 ||
+    endY > 7 ||
+    startX < 0 ||
+    startY < 0 ||
+    endX < 0 ||
+    endY < 0
+  ) {
+    throw new Error("Values cannot be greater than 7 or less than 0");
+  }
+
+  const starting = ChessSpace(startX, startY);
+
+  const queue = [starting];
+  const storage = new Map();
+  let distCounter = 0;
+  queue[0].distance = 0;
+
+  while (queue.length !== 0) {
+    let shifted = queue.shift();
+    if (storage.has(shifted.name) === true) {
+      shifted = queue.shift();
+    }
+
+    const moveList = shifted.knightMoveList();
+    distCounter++;
+
+    if (queue.length === 1) {
+      distCounter = 0;
+    }
+
+    for (let i = 0; i < moveList.length; i++) {
+      if (moveList[i] === undefined) continue;
+      moveList[i].distance = distCounter;
+      moveList[i].prev = shifted;
+      moveList[i].traveled = true;
+      if (moveList[i].xPos === endX && moveList[i].yPos === endY)
+        return travelHelper(moveList[i]);
+      storage.set(moveList[i].name, [moveList[i].xPos, moveList[i].yPos]);
+      queue.push(moveList[i]);
+    }
+    if (storage.size === 64) break;
+  }
+}
+
+knightsTravails([0, 4], [0, 5]);
